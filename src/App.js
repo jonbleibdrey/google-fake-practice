13d517e0c61323079edf2010b0eb29e0449b1c06
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-12-29 11:46:27 -0500

    restareded so added everything from old project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react"
 import './App.css';
+import Home from './pages/Home'
+
 
 function App() {
   return (
+    //BEM
     <div className="App">
-      <h1> google home</h1>
+      <h1>google clone</h1>
+
+      {/*Home google page(the one with the search) */}
+        <Home/>
+      {/*SearchPage(the result page) */}
+
     </div>
   );
 }
 
 export default App;
+
