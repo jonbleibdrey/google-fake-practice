a63c54861af2a9035c226ee768ff4923a583b23c
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-12-31 11:13:17 -0500

    updated input field and state using hooks

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Search.css'
 import SearchIcon from '@material-ui/icons/Search'
 import MicIcon from '@material-ui/icons/Mic'
+import { Button }  from '@material-ui/core'
 
 export default function Search() {
+
+    const [input, setInput] = useState("");
+
+
+    const search = e => {
+        e.preventDefault();
+    } 
+
     return (
         <div className='search'>
             <div className="search__input">
                 <SearchIcon className='search__inputIcon'/>
-                <input />
+                <input value={input} onChange={e => setInput(e.target.value)}/>
                 <MicIcon/>
             </div>
+
+            <div className="search__buttons">
+                <Button onClick={search} variant="outlined"> Google Search</Button>
+                <Button variant="outlined">feeling lucky?</Button>
+            </div>
+
         </div>
     )
 }
