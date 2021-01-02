97ab62647e2f5a73a2b405934657d469c5d9a562
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-01 19:11:57 -0500

    small edit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import './SearchPage.css'
+import React from "react";
+import "./SearchPage.css";
 
 export default function SearchPage() {
-    return (
-        <div className="searchPage">
-           <h1> search page to be contin...</h1>
-        </div>
-    )
+  return (
+    <div className="searchPage">
+      <h1> search page </h1>
+    </div>
+  );
 }
