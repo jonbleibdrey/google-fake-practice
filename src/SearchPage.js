878657787d782e10ea63ba65961e625728431641
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-04 13:49:26 -0500

    added api and it works

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "./SearchPage.css";
 import {useStateValue} from './StateProvider'
+import useGoogleSearch from "./useGoogleSearch";
 
 export default function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  const {data} = useGoogleSearch(term)
 
+
+  console.log(data)
   return (
     <div className="searchPage">
       <div className="searchPage__header">
