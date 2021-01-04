4db0bf1756d37c9108ce5df0e27e90eb1ef741e7
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-04 14:03:12 -0500

    finished up search from api

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import "./SearchPage.css";
 import {useStateValue} from './StateProvider'
 import useGoogleSearch from "./useGoogleSearch";
+import Response from './Response'
+import { Link } from "react-router-dom";
 
 export default function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-  const {data} = useGoogleSearch(term)
+  // LIVE DATA
+  // const {data} = useGoogleSearch(term) 
+
+  const data = Response;
 
 
   console.log(data)
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-        <h1> {term} </h1>
+       <Link to='/'>
+         <img className='searchPage__logo' src="/images/rainbow1.png" alt=""/>
+       </Link>
       </div>
       <div className="searchPage__results">
 
