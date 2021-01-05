5c9a38515a47187660ddb517225f424eb3de0924
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-05 14:09:41 -0500

    created link tags search bar options

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,6 +5,12 @@ import useGoogleSearch from "./useGoogleSearch";
 import Response from './Response'
 import { Link } from "react-router-dom";
 import Search from './Search'
+import SearchIcon from '@material-ui/icons/Search'
+import DescriptionIcon from '@material-ui/icons/Description'
+import ImageIcon from '@material-ui/icons/Image'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
+import RoomIcon from '@material-ui/icons/Room'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 export default function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -21,11 +27,42 @@ export default function SearchPage() {
        <Link to='/'>
          <img className='searchPage__logo' src="/images/rainbow1.png" alt=""/>
        </Link>
-      </div>
-
       <div className='searchPage__headerBody'>
         <Search hideButtons/>
+        <div className='searchPage__options'>
+          <div className='searchPage__optionsLeft'> 
+
+            <div className='searchPage__option'>
+              <SearchIcon/>
+              <Link to='/all'>ALL</Link>
+            </div>
+            <div className='searchPage__option'>
+              <DescriptionIcon/>
+              <Link to='/news'>NEWS</Link>
+            </div>
+            <div className='searchPage__option'>
+              <ImageIcon/>
+              <Link to='/images'>IMAGES</Link>
+            </div>
+            <div className='searchPage__option'>
+              <LocalOfferIcon/>
+              <Link to='/shopping'>SHOPPING</Link>
+            </div>
+            <div className='searchPage__option'>
+              <RoomIcon/>
+              <Link to='/maps'>MAPS</Link>
+            </div>
+            <div className='searchPage__option'>
+              <MoreVertIcon/>
+              <Link to='/more'>MORE</Link>
+            </div>
+
+          </div>
+
+        </div>
       </div>
+    </div>
+
 
       <div className="searchPage__results">
 
